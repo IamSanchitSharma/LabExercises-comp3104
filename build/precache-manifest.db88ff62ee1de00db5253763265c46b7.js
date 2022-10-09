a02773d2a72a2e3cd5fa30a9aee081bf530fac9f
IamSanchitSharma <sanchit12sharma@gmail.com>
2022-10-09 16:26:53 -0400

    React project code added

---
+++ build/precache-manifest.db88ff62ee1de00db5253763265c46b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dfb2192c6707f8d1765",
+    "url": "/IamSanchitSharma/LabExercises-comp3104/static/css/main.758e7583.chunk.css"
+  },
+  {
+    "revision": "5dfb2192c6707f8d1765",
+    "url": "/IamSanchitSharma/LabExercises-comp3104/static/js/main.5dfb2192.chunk.js"
+  },
+  {
+    "revision": "c6b79cab6799b4eb8611",
+    "url": "/IamSanchitSharma/LabExercises-comp3104/static/js/runtime~main.c6b79cab.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/IamSanchitSharma/LabExercises-comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "7367b511038bd0e1f856db6b10e07e9e",
+    "url": "/IamSanchitSharma/LabExercises-comp3104/index.html"
+  }
+];
